30f59c3069910ba3a9e50f6032fbc413570fd48d
uzichoi <cjiyou1090@gmail.com>
2026-05-29 12:43:30 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void print10Stars() {
+    for(int i=0; i<10; i++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+int main() {
+    // Please write your code here.
+    for(int i=0; i<5; i++){
+        print10Stars();
+    }
+
+    return 0;
+}
